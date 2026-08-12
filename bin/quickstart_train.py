3e25511f67b82837cfc1370b5c6d8ee367db2655
Xavier Wilson <xiwilson05@gmail.com>
2026-08-11 20:25:20 -0400

    Update quickstart_train.py

--- bin/quickstart_train.py
+++ bin/quickstart_train.py
@@ -3,7 +3,7 @@ from hazdet.train import hyperparameter_tune_all_models
 # e.g., X,y =  create_features(file='Tweet annotation.csv')
 
 # This is your training data
-filename ='../ground_truth_data/TweetGT.csv'
+filename ='../ground_truth_data/training_data_300.csv'
 # saves best model as 'finalized_model_'+best_model+'.sav', 
 # where best model could be NN (feed-forward neural network), RF (random forest), XGB (XGBoost), or SVM (support vector machine)
 # Also saves performance (ROC-AUC) of each candidate model in a pickle file
