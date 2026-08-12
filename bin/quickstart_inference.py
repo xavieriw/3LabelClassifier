ffecff4c69cce83ea1bd2def4116bfa280a7fd7f
Xavier Wilson <xiwilson05@gmail.com>
2026-08-11 20:36:14 -0400

    Add files via upload

--- bin/quickstart_inference.py
+++ bin/quickstart_inference.py
@@ -1,13 +1,15 @@
+import sys
 from hazdet.inference import inference
-from glob import glob
-def main():
-    # We assume that text is in a column "text"
-    filename = 'text.csv'
-    model_filename = list(glob('../*/*.sav'))[0]
-    # Output: Pandas dataframe of file in text.csv with additional column called "hazard", 
-    # which lists the hazard confidence for text in each row
-    predictions = inference(filename,model_filename)
-    
 
-__init__():
-    main()
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: python quickstart_inference.py <input_file> <model_filename> [output_file]')
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    model_filename = sys.argv[2]
+    output_file = sys.argv[3] if len(sys.argv) > 3 else 'predictions.csv'
+
+    result = inference(input_file, model_filename)
+    result.to_csv(output_file, index=False)
+    print(f'Saved {len(result)} predictions to {output_file}')
